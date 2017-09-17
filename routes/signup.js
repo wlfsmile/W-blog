a7a3739de5b829a958e14908fd59acdcbfaf181e
wlfsmile <wlffsmile@gmail.com>
2017-09-17 21:35:22 +0800

    个人博客第一版

---
+++ routes/signup.js
@@ -0,0 +1,26 @@
+var path = require('path');
+var sha1 = require('sha1');
+var express = require('express');
+var router = express.Router();
+
+var UserModel = require('../models/users');
+var checkNotLogin = require('../middlewares/check').checkNotLogin;
+
+// GET /signup 注册页
+router.get('/', checkNotLogin, function(req, res, next) {
+  res.render('signup');
+});
+
+// POST /signup 用户注册
+router.post('/', checkNotLogin, function(req, res, next) {
+  var name = req.fields.name;
+  var gender = req.fields.gender;
+  var bio = req.fields.bio;
+  var avatar = req.files.avatar.path.split(path.sep).pop();
+  var password = req.fields.password;
+  var repassword = req.fields.repassword;
+
+  
+});
+
+module.exports = router;
